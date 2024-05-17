776fcf6bd05c48957bf2d64099649af18345ab2e
Westsi <76999267+Westsi@users.noreply.github.com>
2024-05-17 20:30:01 +0100

    moved files, created notification screen

---
+++ smartwatch_code/include/screens/notificationscreen.h
@@ -0,0 +1,18 @@
+#ifndef NOTIFICATIONSCREEN_H_
+#define NOTIFICATIONSCREEN_H_
+
+#include "touchscreen.h"
+#include <TFT_eSPI.h>
+
+class NotificationScreen : public Screen {
+    public:
+        void init(TFT_eSprite* spr, int width, int height);
+        void update();
+        void render();
+        void registerInteractionHandler(InteractionCallback callback, int minx, int maxx, int miny, int maxy);
+        void registerInteractionHandler(InteractionHandler ih);
+        void handleInteraction(String gesture, int x, int y);
+};
+
+
+#endif
