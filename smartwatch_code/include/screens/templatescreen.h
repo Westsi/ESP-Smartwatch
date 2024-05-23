420d66ac702456d2629e27821d862b50c7483d05
Westsi <76999267+Westsi@users.noreply.github.com>
2024-05-23 19:10:13 +0100

    added template screen

---
+++ smartwatch_code/include/screens/templatescreen.h
@@ -0,0 +1,20 @@
+#ifndef SCREENNAME_H_
+#define SCREENNAME_H_
+
+#include "touchscreen.h"
+#include <TFT_eSPI.h>
+
+class SCREENNAME : public Screen {
+    public:
+        void init(TFT_eSprite* spr, int width, int height);
+        void update();
+        void render();
+        void registerInteractionHandler(InteractionCallback callback, int minx, int maxx, int miny, int maxy);
+        void registerInteractionHandler(InteractionHandler ih);
+        void handleInteraction(String gesture, int x, int y);
+};
+
+extern SCREENNAME sn;
+
+
+#endif
