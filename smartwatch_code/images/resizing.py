4484a93d855d2f84ac422f66789a5d2a14757511
Westsi <76999267+Westsi@users.noreply.github.com>
2024-06-04 15:41:34 +0100

    actually good images

--- smartwatch_code/images/resizing.py
+++ smartwatch_code/images/resizing.py
@@ -4,13 +4,13 @@ files = glob.glob("M:\Custom_Smartwatch\smartwatch_code\images\*.png")
 # files = [r"M:\Custom_Smartwatch\smartwatch_code\images\skip_next_48.png"]
 
 for file in files:
-    if file.startswith("Spotify") or "_160" in file:
+    if file.startswith("Spotify") or "_160" in file or "_24" in file:
         continue
     im = Image.open(file)
 
-    fname = (file.split("\\")[-1]).split("_")[0] + "_160.png"
+    fname = (file.split("\\")[-1]).split("_")[0] + "_48.png"
     # fname = "skip_next_160.png"
 
-    newsize = (160, 160)
+    newsize = (48, 48)
     im1 = im.resize(newsize)
     im1.save(fname, "PNG")
