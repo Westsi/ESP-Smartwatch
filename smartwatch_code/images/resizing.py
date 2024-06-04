b685bd0d182c9d8c4f0931e0cd3274701e227e9e
Westsi <76999267+Westsi@users.noreply.github.com>
2024-06-04 14:59:56 +0100

    renamed images and generated 160 versions

--- smartwatch_code/images/resizing.py
+++ smartwatch_code/images/resizing.py
@@ -1,14 +1,16 @@
 from PIL import Image
 import glob
 files = glob.glob("M:\Custom_Smartwatch\smartwatch_code\images\*.png")
+# files = [r"M:\Custom_Smartwatch\smartwatch_code\images\skip_next_48.png"]
 
 for file in files:
-    if file.startswith("Spotify"):
+    if file.startswith("Spotify") or "_160" in file:
         continue
     im = Image.open(file)
 
-    fname = (file.split("\\")[-1]).split("_")[0] + "_160"
+    fname = (file.split("\\")[-1]).split("_")[0] + "_160.png"
+    # fname = "skip_next_160.png"
 
     newsize = (160, 160)
     im1 = im.resize(newsize)
-    im1.save(fname)
+    im1.save(fname, "PNG")
